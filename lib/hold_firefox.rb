51a7d11dc205db44a25b84d04ae3cccb73a46fa9
Danilo Jeremias da Silva <danilo.silva@euax.com.br>
2016-12-16 13:18:32 -0200

    DRY

--- lib/hold_firefox.rb
+++ lib/hold_firefox.rb
@@ -6,7 +6,7 @@ module HoldFirefox
 end
 
 if RUBY_PLATFORM =~ /linux/i
-  Selenium::WebDriver::Firefox::Binary.path = Selenium::WebDriver::Firefox::Binary.path = Pathname.new(File.dirname(__FILE__)).join('..', 'ext', 'hold_firefox', 'firefox', 'firefox-bin').to_s
+  Selenium::WebDriver::Firefox::Binary.path = Pathname.new(File.dirname(__FILE__)).join('..', 'ext', 'hold_firefox', 'firefox', 'firefox-bin').to_s
 elsif RUBY_PLATFORM =~ /darwin/i
-  Selenium::WebDriver::Firefox::Binary.path = Selenium::WebDriver::Firefox::Binary.path = Pathname.new(File.dirname(__FILE__)).join('..', 'ext', 'hold_firefox', 'Firefox.app', 'Contents', 'MacOS', 'firefox-bin').to_s
+  Selenium::WebDriver::Firefox::Binary.path = Pathname.new(File.dirname(__FILE__)).join('..', 'ext', 'hold_firefox', 'Firefox.app', 'Contents', 'MacOS', 'firefox-bin').to_s
 end
